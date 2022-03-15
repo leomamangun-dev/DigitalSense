d3141c0b4d09a769a1ce30b880d0bdd85e43e137
Leo Paul Mamangun <mamangunleopaul01@gmail.com>
2022-03-15 15:42:04 +0800

    homepage

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -8,11 +8,32 @@ AppName.Modules.ThemeModule = (function () {
   const _privateMethod = () => {
     // private stuff
 
-    const swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });
+
+      var btn = $('.scroll-top');
+
+      $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+          btn.addClass('show');
+        } else {
+          btn.removeClass('show');
+        }
+      });
+  
+      btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+      });
+
+      $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            $(".navbar").addClass("active");
+        } else {
+            $(".navbar").removeClass("active");
+        }
+      });
+    
   };
 
   /////////////////////
